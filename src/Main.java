6a167c5a7e7d91ce3db426be0d56a43c13d0c054
NokKbl <asdfghjklnokky@gmail.com>
2018-01-12 20:51:44 +0700

    just change upper limit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@
 public class Main {
 	public static void main(String[] args) {
 		// upper limit for secret number in guessing game
-		int upperBound = 100;
+		int upperBound = 144;
 		NumberGame game = new KunyarukGame(upperBound);
 		GameConsole ui = new GameConsole( );
 		int solution = ui.play( game );
